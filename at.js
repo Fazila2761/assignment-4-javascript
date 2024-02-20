f233291f752a6f6c718ccdff847439d5b02e7681
Fazila2761 <fazilakhan2761@gmail.com>
2024-02-20 14:15:10 +0600

    first commit

---
+++ at.js
@@ -0,0 +1,48 @@
+// --------------------------if else--------------------
+
+var result = (79);
+aplus =80;
+a_mynus=79;
+
+if(result==aplus){
+    console.log('he got a plus')
+}
+else 
+console.log("he can't achive a grade")
+// -----------------------------
+
+// -----------------------------else if--------------------------
+var whatday = 'monday';
+if (whatday === 'friday'){
+console.log('it is holiday at bangladesh')} 
+
+else if (whatday === 'sunday'){
+console.log('it is holiday at india')
+}
+
+else if (whatday === 'wednesday'){
+console.log('it is also a holiday worldwide because this day is international mother language day')} 
+
+else {
+  console.log ('It normal working day today')
+}
+// ---------------------nested-----------------------------
+
+
+  var  lipstic= 100;
+var myBudget = 110;
+huda=true;
+
+
+if(lipstic < myBudget){
+    if(huda == false ){
+        console.log('huda brand kinbo na');
+    }else{
+        console.log("onno brand kinbo");
+    }
+    
+}
+
+else{
+    console.log("kajol kinbo")
+}
